73bb9e47bdab63c1997a0a95205961e7d6a94f43
nandithav03 <nandithavgowda03@gmail.com>
2025-04-12 19:04:05 +0530

    second commit

--- java/src/loops/factorial.java
+++ java/src/loops/factorial.java
@@ -1,5 +1,18 @@
 package loops;
 
+import java.util.Scanner;
+
 public class factorial {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the number");
+		int num=scanner.nextInt();
+		int fact=1;
+		while(num!=0) {
+			 fact=fact*num;
+			 num--;
+		}
+		System.out.println("the factorial of given number is:" +fact);
+		}
 
 }
